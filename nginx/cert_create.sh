07d3b14a86376dd67bce251055c4a5776ef5a8a9
dni ⚡ <office@dnilabs.com>
2025-04-08 15:51:21 +0200

    fix: no www domain

--- nginx/cert_create.sh
+++ nginx/cert_create.sh
@@ -5,5 +5,5 @@ if [ -z "$domain" ]; then
   exit 1
 fi
 cat ./template.conf | sed "s/%domain%/$domain/g" > ./conf/app.conf
-docker compose run --rm certbot certonly --webroot --webroot-path /var/www/certbot/ --dry-run -d $domain -d www.$domain
+docker compose run --rm certbot certonly --webroot --webroot-path /var/www/certbot/ --dry-run -d $domain
 docker compose restart
