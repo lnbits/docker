5f6ef2a8d40ef01bed1ca4bf6da8abd121391348
dni ⚡ <office@dnilabs.com>
2025-04-08 16:47:28 +0200

    refactor: nginx tempaltes

--- nginx/cert_create.sh
+++ nginx/cert_create.sh
@@ -4,6 +4,9 @@ if [ -z "$domain" ]; then
   echo "Usage: $0 <domain>"
   exit 1
 fi
-cat ./template.conf | sed "s/%domain%/$domain/g" > ./conf/app.conf
+cat ./http.conf | sed "s/%domain%/$domain/g" > ./conf/http.conf
+docker compose restart
 docker compose run --rm certbot certonly --webroot --webroot-path /var/www/certbot/ --dry-run -d $domain
+docker compose run --rm certbot certonly --webroot --webroot-path /var/www/certbot/ -d $domain
+cat ./https.conf | sed "s/%domain%/$domain/g" > ./conf/https.conf
 docker compose restart
